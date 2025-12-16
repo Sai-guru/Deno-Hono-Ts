52e7757894c567d6c13f51218a0deb1622a271c9
Sai-guru <lpnkaveri@gmail.com>
2025-12-16 19:12:32 +0530

    basics

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-10 text-center">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <h1 className="text-4xl font-bold">Welcome to Next.js! buddy</h1>
       <p className="text-lg text-amber-400">This is the home page of your Next.js application.</p>
      
     </main>
