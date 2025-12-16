7313f3a28152781fbc9393d6c498991105c27992
Sai-guru <lpnkaveri@gmail.com>
2025-12-16 16:55:55 +0530

    setted up nextjs[tsx] using bun

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-10 text-center">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <p className="text-lg text-amber-400">This is the home page of your Next.js application.</p>
+     
+    </main>
+  );
+}
